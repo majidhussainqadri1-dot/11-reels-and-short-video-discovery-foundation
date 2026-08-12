c6b0e4524dbe5ed38379409c0b4512163c5ac67b
majidhussainqadri1-dot <majidhussainqadri1@gmail.com>
2026-08-12 19:27:54 +0500

    Load third review privacy hardening

--- 11-reels-foundation/11-reels-foundation.php
+++ 11-reels-foundation/11-reels-foundation.php
@@ -54,6 +54,7 @@ $files = array(
 	'class-rsv-fresh20-hardening.php',
 	'class-rsv-fresh-review-hardening.php',
 	'class-rsv-fresh-review-public-minimization.php',
+	'class-rsv-third-review-hardening.php',
 	'trait-rsv-top20-context.php',
 	'trait-rsv-top20-stories.php',
 	'trait-rsv-top20-responses.php',
